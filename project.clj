69ab2d4b0af1871ed63c719ad679f80da2071874
maverick <maverick@factual.com>
2015-05-12 23:00:50 +0800

    remove foursquare google and yelp related code

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject factual/c4 "0.2.0"
+(defproject factual/c4 "0.2.1-SNAPSHOT"
   :description "Convenience features for handling record files the Clojure way"
   :url "https://github.com/Factual/c4"
   :license {:name "Eclipse Public License"
@@ -9,14 +9,9 @@
                  [com.cemerick/pomegranate "0.0.13"]
                  [factql "1.0.4"]
                  [clojure-csv/clojure-csv "2.0.0-alpha2"]
-                 [mavericklou/foursquare-api "1.0.3"]
                  [cheshire "5.0.2"]
                  [ordered "1.3.1"]
                  [retry "1.0.2"]
-                 ;;;; for yelp lib
-                 [org.scribe/scribe "1.2.0"]
-                 ;;;; for google places API
-                 [com.google.api-client/google-api-client "1.8.0-beta"]
                  [org.apache.httpcomponents/httpclient "4.2.3"]]
   :aliases {"test!" ["do" "clean," "deps," "test" ":all"]}
   :aot :all)
